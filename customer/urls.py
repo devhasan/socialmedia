cb5bba8126c00de0ebd4b4342655b549a1522560
Hasan Mahmud <47460273+devhasan@users.noreply.github.com>
2025-02-23 04:33:20 -0800

    Delete customer directory

--- customer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/customer/', views.customer),
-    path('admin/customerDetails/', views.customerDetails),
-    path('admin/contact/', views.contact),
-]
